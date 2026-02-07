707e0905f89f1bb70c4e8064ee5ebef94819aa54
MD <animico@gmail.com>
2026-02-07 00:32:04 -0800

    se quito lo del watning de middleware

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const token = request.cookies.get("nettnett_session")?.value;
   const isLoginPage = request.nextUrl.pathname === "/login";
   const isDashboard = request.nextUrl.pathname.startsWith("/dashboard");
